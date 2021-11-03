4c8621e4de47a728753955235ae65e41414989ec
Mansirov Senan <mansirovsenan2@gmail.com>
2021-11-03 18:44:52 +0400

    update

---
+++ Work/Python/week7-day3/func.py
@@ -0,0 +1,40 @@
+# Praqram daxilində hər hansı bir əməliyyatın yerinə yetirilməsi üçün yazılan kamandalar ardıcılığına  Funksiya deyilir.
+# Funksiya parametrli ve ya parametrsiz olur.
+# Funksiyanı yaratmaq üçün def açar sözünən istifadə olunur
+    #   def  funksiya adı (parametr1, paramet2,....)
+        #          operatorlar 
+    #   		return qiymət
+
+
+
+def f ():    
+ 
+ print("salam")
+
+f()
+
+
+
+
+
+def func():
+    return(33)
+func()
+
+
+
+
+
+
+def hesabla(a, b):
+    x=a+b
+    if a>b:
+        x=x+a
+    else:
+        x=x-b
+    x=x+10
+    print(x)
+hesabla(3,5)
+
+
+
