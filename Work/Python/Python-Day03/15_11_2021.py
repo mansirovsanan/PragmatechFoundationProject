4b63e9815696f3e639aab095c2c324cb0e9f0ec7
Mansirov Senan <mansirovsenan2@gmail.com>
2021-12-06 18:50:04 +0400

    task02

--- Work/Python/Python-Day03/15_11_2021.py
+++ Work/Python/Python-Day03/15_11_2021.py
@@ -11,34 +11,34 @@ adlar=[
 
 # Butun adlari ekrana cap eden metod yazin adlariGoster()
 
-def adlariGoster():
-    for ad in adlar:
-        print(ad)
-adlariGoster()
+# def adlariGoster():
+#     for ad in adlar:
+#         print(ad)
+# adlariGoster()
 
 
 # Listin cut yerde duran elementlerini gosteren metod yazin cutYerdekiler()
-def cutYerdekiler():
-    for ad in range(len(adlar)):
-        if ad%2==0:
-            print(adlar[ad])
-cutYerdekiler()
+# def cutYerdekiler():
+#     for ad in range(len(adlar)):
+#         if ad%2==0:
+#             print(adlar[ad])
+# cutYerdekiler()
 
 # Listi elifba sirasina gore siralayan metod yazin listiSirala()
 
-def listiSirala(adlar):
-    adlar.sort()
-    print(adlar)
-listiSirala(adlar)
+# def listiSirala(adlar):
+#     adlar.sort()
+#     print(adlar)
+# listiSirala(adlar)
 
 # Daxilində e herfi olan adları ekrana cap edən metod yazin
 
-def herfTap():
-    for ad in adlar :
-        for i in ad:
-            if i=="e":
-                print(ad)
-herfTap()
+# def herfTap():
+#     for ad in adlar :
+#         for i in ad:
+#             if i=="e":
+#                 print(ad)
+# herfTap()
 
 # Son hərfi i olan adlari ekrana cap eden metod yazin
 
